1fb63a6757a6c9fb94c7ce38a21476f6159a85b0
lukasyano <toliusislukas@yahoo.com>
2024-04-27 14:34:47 +0300

    Initial Commit

---
+++ Homework/HomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeworkApp.swift
+//  Homework
+//
+//  Created by Lukas Toliusis on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct HomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
