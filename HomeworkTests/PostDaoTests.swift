bbcf02464adfcc3a250e3bfeac1519e98423b312
lukasyano <toliusislukas@yahoo.com>
2024-05-06 09:25:26 +0300

    api mapper, dao tests

--- HomeworkTests/PostDaoTests.swift
+++ HomeworkTests/PostDaoTests.swift
@@ -1,13 +1,14 @@
 //
-//  HomeworkTests.swift
+//  PostDaoTests.swift
 //  HomeworkTests
 //
-//  Created by Lukas Toliusis on 04/05/2024.
+//  Created by Lukas Toliusis on 05/05/2024.
 //
 
 import XCTest
 
-final class HomeworkTests: XCTestCase {
+final class PostDaoTests: XCTestCase {
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -26,8 +27,9 @@ final class HomeworkTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        measure {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
+
 }
