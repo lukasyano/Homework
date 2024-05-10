f8c7221fdbe68d4807bc9409bf336ce4fe1dc97b
lukasyano <toliusislukas@yahoo.com>
2024-05-10 14:32:30 +0300

    remove tracking .dstrore

--- Homework/UI/MainScreen.swift
+++ Homework/UI/MainScreen.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct MainScreen: View {
     @EnvironmentObject var dependencyContainer: DependencyContainer
-    
+
     var body: some View {
         PostsScreenView(viewModel: dependencyContainer.makePostsViewModel())
     }
